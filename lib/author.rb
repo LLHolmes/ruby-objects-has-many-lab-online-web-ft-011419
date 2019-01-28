d4530c1ea2005d42ffde35f671c808bd2acdf890
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-28 16:19:58 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,4 +11,14 @@ class Author
     article.author = self
   end
   
+  def add_post_by_title(title)
+    article = Post.new(title)
+    @posts << article
+    article.author = self
+  end
+  
+  def self.post_count
+    Post.all.length
+  end
+  
 end
