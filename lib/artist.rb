828553af704220a5b29fa72e3e7f1fc7f0926745
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-28 17:31:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,9 +6,9 @@ class Artist
     @songs = []
   end
   
-  def add_song(song)
-    @songs << song
-    song.artist = self
+  def add_song(title)
+    @songs << title
+    title.artist = self
   end
   
   def add_song_by_name(song_name)
